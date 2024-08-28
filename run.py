b1079b5cd16ce7fd94a07c406f2ac9bee7b5c821
Robert Divkovic <robertdivkovic98@gmail.com>
2024-08-28 22:48:11 +0000

    Fix errors and bugs

--- run.py
+++ run.py
@@ -29,14 +29,13 @@ def choose_level():
     print("3: Challenging (1-1000)")
 
     while True:
-        choice = input("Enter the number corresponding to your cohoice: ")
+        choice = input("Enter the number corresponding to your choice: ")
         if choice in levels:
-            print(f"Your have choosen the {levels[choice]} level.")
+            print(f"Your have chosen the {levels[choice]} level.")
             return SHEET.worksheet(levels[choice])
         else:
             print("Invalid choice, please enter 1, 2, or 3.")
 
-level_sheet = choose_level()
 
 def play_game(level_sheet, level):
     """
@@ -57,7 +56,7 @@ def play_game(level_sheet, level):
     while True:
         try:
             guess = int(input("Enter your guess: "))
-            guesses =+ 1
+            guesses += 1
             if guess == target:
                 print("Congrats, you guessed the wanted number!")
                 break
@@ -71,7 +70,7 @@ def play_game(level_sheet, level):
     return guesses
 
 level_sheet = choose_level()
-number_of_guesses = play_game(level_sheet, level)
+number_of_guesses = play_game(level_sheet, 'easy')
 
 def store_result(guesses_sheet, player_name, number_of_guesses):
     """
@@ -121,7 +120,7 @@ def display_summary(guesses_sheet, player_name):
     retrieving their session data from 'guesses' sheet.
     """
     # get all datra from the sheet
-    record = guesses_sheet.get_all_records()
+    records = guesses_sheet.get_all_records()
 
     # Findthe records for player
     player_records = [record for record in records if record['Player Name'] == player_name]
@@ -139,7 +138,7 @@ def main():
         level_sheet = choose_level()
         level = level_sheet.title # Get level name
         player_name = input("Enter your name: ")
-        number_of_guesses = player_game(level_sheet, level)
+        number_of_guesses = play_game(level_sheet, level)
 
         # store the result in the guesses sheet
         store_result(guesses_sheet, player_name, number_of_guesses)
